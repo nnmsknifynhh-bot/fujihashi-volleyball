f06db3609e37d509f324e636538bf2271029e522
三浦早織 <nnmsknifynhh@gmail.com>
2026-05-31 15:27:32 +0000

    Add: 全選手確認ダイアログ追加 - teamフィールドの実際の値を表示してチーム修正可能に

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4014325987"
+    serviceWorkerVersion: "1104236446"
   }
 });
